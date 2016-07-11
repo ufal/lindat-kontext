a2cabd2fb2b8c347c7eecbc3fa08b96221ac05ff
jm <jm@server>
2016-07-11 21:36:49 +0200

    new grunt version complain?!

--- scripts/grunt/kontext.js
+++ scripts/grunt/kontext.js
@@ -23,8 +23,8 @@
 (function (module) {
     'use strict';
 
-    let fs = require('fs');
-    let merge = require('merge');
+    var fs = require('fs');
+    var merge = require('merge');
 
     function camelizeName(s) {
         return s.split('_')
@@ -33,10 +33,10 @@
     }
 
     function findPluginTags(doc) {
-        let plugins = doc.getElementsByTagName('plugins');
-        let ans = [];
+        var plugins = doc.getElementsByTagName('plugins');
+        var ans = [];
         function findJsModule(root) {
-            let jsm = root.getElementsByTagName('js_module');
+            var jsm = root.getElementsByTagName('js_module');
             if (jsm.length > 0) {
                 return jsm[0].textContent;
             }
@@ -44,10 +44,10 @@
         }
 
         if (plugins) {
-            for (let i = 0; i < plugins[0].childNodes.length; i += 1) {
-                let node = plugins[0].childNodes[i];
+            for (var i = 0; i < plugins[0].childNodes.length; i += 1) {
+                var node = plugins[0].childNodes[i];
                 if (node.nodeType === 1) {
-                    let jsm = findJsModule(node);
+                    var jsm = findJsModule(node);
                     ans.push({canonicalName: camelizeName(node.nodeName), jsModule: jsm ? jsm : null});
                 }
             }
@@ -59,9 +59,9 @@
         function endsWith(s, subs) {
             return s.indexOf(subs) === s.length - subs.length;
         }
-        let ans = [];
+        var ans = [];
         findPluginTags(doc).forEach((item) => {
-            let dirPath;
+            var dirPath;
             if (item['jsModule']) {
                 dirPath = pluginDir + '/' + item['jsModule'];
                 fs.readdirSync(dirPath).forEach(function (filename) {
@@ -79,18 +79,18 @@
     }
 
     function findAllThemeCss(doc) {
-        let kontextNode = doc.getElementsByTagName('kontext')[0];
-        let themeNode = null;
-        let cssNode = null;
-        let styles = [];
+        var kontextNode = doc.getElementsByTagName('kontext')[0];
+        var themeNode = null;
+        var cssNode = null;
+        var styles = [];
 
-        let themeName = null;
-        let srch = kontextNode.getElementsByTagName('name')[0];
+        var themeName = null;
+        var srch = kontextNode.getElementsByTagName('name')[0];
         if (srch) {
             themeName = srch.textContent.trim();
         }
 
-        for (let i = 0; i < kontextNode.childNodes.length; i += 1) {
+        for (var i = 0; i < kontextNode.childNodes.length; i += 1) {
             if (kontextNode.childNodes[i].nodeName === 'theme') {
                 themeNode = kontextNode.childNodes[i];
                  break;
@@ -99,9 +99,9 @@
         if (themeNode) {
             cssNode = themeNode.getElementsByTagName('css')[0];
             if (cssNode) {
-                for (let i = 0; i < cssNode.childNodes.length; i += 1) {
+                for (var i = 0; i < cssNode.childNodes.length; i += 1) {
                     if (cssNode.childNodes[i].nodeType === 1 || cssNode.childNodes[i].nodeType === 3) {
-                        let cssPath = cssNode.childNodes[i].textContent.trim();
+                        var cssPath = cssNode.childNodes[i].textContent.trim();
                         if (cssPath && resourceIsLocal(cssPath)) {
                             styles.push('public/files/themes/' + themeName + '/'
                                 + cssNode.childNodes[i].textContent.trim());
@@ -118,9 +118,9 @@
      * build process with the rest of CSS.
      */
     module.exports.getCustomStyles = function (confPath, pluginsPath) {
-        let xmldom = require('xmldom');
-        let data = fs.readFileSync(confPath, {encoding: 'utf8'});
-        let doc = new xmldom.DOMParser().parseFromString(data);
+        var xmldom = require('xmldom');
+        var data = fs.readFileSync(confPath, {encoding: 'utf8'});
+        var doc = new xmldom.DOMParser().parseFromString(data);
         return findAllThemeCss(doc).concat(findAllPluginCss(pluginsPath, doc));
     };
 
@@ -134,12 +134,12 @@
      * @return {[fakePath:string]:string}
      */
     module.exports.loadModulePathMap = function (confPath, isProduction) {
-        let data = fs.readFileSync(confPath, {encoding: 'utf8'});
-        let DOMParser = require('xmldom').DOMParser;
-        let doc = new DOMParser().parseFromString(data);
-        let reactModule = isProduction ? 'vendor/react.min' : 'vendor/react.dev';
-        let reactDomModule = isProduction ? 'vendor/react-dom.min' : 'vendor/react-dom.dev';
-        let pluginMap = {
+        var data = fs.readFileSync(confPath, {encoding: 'utf8'});
+        var DOMParser = require('xmldom').DOMParser;
+        var doc = new DOMParser().parseFromString(data);
+        var reactModule = isProduction ? 'vendor/react.min' : 'vendor/react.dev';
+        var reactDomModule = isProduction ? 'vendor/react-dom.min' : 'vendor/react-dom.dev';
+        var pluginMap = {
             'win' : 'empty:',
             'conf' : 'empty:',
             'plugins/applicationBar/toolbar': 'empty:',
@@ -162,7 +162,7 @@
      * party libs merged into a single file
      */
     module.exports.listPackedModules = function (isProduction) {
-        let modules = [
+        var modules = [
             'jquery',
             'popupbox',
             'vendor/rsvp',
@@ -196,14 +196,14 @@
      * @return Array<string>
      */
     module.exports.listAppModules = function (tplDir) {
-        let ans = [];
+        var ans = [];
 
         function isExcluded(p) {
             return ['document.js'].indexOf(p) > -1;
         }
 
         fs.readdirSync(tplDir).forEach(function (item) {
-            let srch = /^(.+)\.[jt]s$/.exec(item);
+            var srch = /^(.+)\.[jt]s$/.exec(item);
             if (srch && !isExcluded(item)) {
                 ans.push({
                     name: 'tpl/' + srch[1],
@@ -215,9 +215,9 @@
     };
 
     function findAllMessageFiles(startDir) {
-        let ans = [];
+        var ans = [];
         fs.readdirSync(startDir).forEach((item) => {
-            let fullPath = startDir + '/' + item;
+            var fullPath = startDir + '/' + item;
             if (fs.lstatSync(fullPath).isDirectory() && ['min'].indexOf(item) === -1) {
                 ans = ans.concat(findAllMessageFiles(fullPath));
 
@@ -229,8 +229,8 @@
     }
 
     module.exports.mergeTranslations = function (startDir, destFile) {
-        let files = findAllMessageFiles(startDir);
-        let translations = {};
+        var files = findAllMessageFiles(startDir);
+        var translations = {};
         files.forEach((item) => {
            translations = merge.recursive(translations, JSON.parse(fs.readFileSync(item)));
         });
