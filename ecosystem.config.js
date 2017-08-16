f2ab09143408a385f8877d178b8201509cf084f0
jm <jm@server>
2017-08-16 11:38:39 +0200

    windows friendly pm2 deploy to quest, requires ssh tunnel to kontext-dev and a user specified in DPUSER env. variable

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps:[
+          {
+              "exec_mode": "fork_mode",
+              "cwd": "./",
+              "script": "public/app.py",
+              "name": "kontext" + (process.env.DPNAME || ""),
+              "autorestart": true,
+              "exec_interpreter": "python",
+              "args": "--address 0.0.0.0 --port " + (process.env.PORT || 5000),
+              "kill_timeout": 3200
+          }
+      ],
+      "deploy" : {
+          "staging" : {
+              "user" : process.env.DPUSER || "kontext",
+              "host" : [
+                  {
+                      "host": "127.0.0.1",
+                      "port": "8877"
+                  }
+                ],
+              "repo" : "https://github.com/ufal/lindat-kontext.git",
+              "ref"  : "origin/ci-dev",
+              "path" : "/opt/kontext-staging/deploy",
+              "pre-deploy-local": "echo 'execute ssh -L8877:kontext-dev:22 USER@quest.ms.mff.cuni.cz before this if ssh connection fails, config.xml must be also present!'",
+              "post-deploy" : "npm install && grunt devel && DPNAME=-staging PORT=10001 pm2 startOrRestart ecosystem.config.js",
+              "env" : {
+                "NODE_ENV": "staging",
+              }
+          }
+      }
+};
