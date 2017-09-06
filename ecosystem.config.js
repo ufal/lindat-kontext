9dd62da7e74f39a504832df8a66d166d3229620a
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2017-09-06 16:33:19 +0200

    few corrections

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
               "path" : "/opt/kontext-staging/deploy",
               "pre-deploy-local": "ssh -fN -L8877:kontext-dev:22 " + process.env.DPUSER + "@quest.ms.mff.cuni.cz",
               "post-deploy" : "npm install && grunt production && DPNAME=-staging PORT=10001 pm2 startOrRestart ecosystem.config.js",
-              "post-deploy-local" : "lsof -t -i :8878 | xargs kill -9 ",
+              "post-deploy-local" : "lsof -t -i :8877 | xargs kill -9 ",
               "env" : {
                 "NODE_ENV": "staging",
               }
@@ -58,8 +58,8 @@ module.exports = {
               "ref"  : "origin/kontext-dev",
               "path" : "/opt/kontext-kira/deploy",
               "pre-deploy-local": "ssh -fN -L8877:kontext-dev:22 " + process.env.DPUSER + "@quest.ms.mff.cuni.cz",
-              "post-deploy" : "npm install && grunt devel && DPNAME=-jm PORT=10005 pm2 startOrRestart ecosystem.config.js",
-              "post-deploy-local" : "lsof -t -i :8878 | xargs kill -9 ",
+              "post-deploy" : "npm install && grunt devel && DPNAME=-kira PORT=10005 pm2 startOrRestart ecosystem.config.js",
+              "post-deploy-local" : "lsof -t -i :8877 | xargs kill -9 ",
               "env" : {
                 "NODE_ENV": "staging",
               }
@@ -77,7 +77,7 @@ module.exports = {
               "path" : "/opt/kontext-jm/deploy",
               "pre-deploy-local": "ssh -fN -L8877:kontext-dev:22 " + process.env.DPUSER + "@quest.ms.mff.cuni.cz",
               "post-deploy" : "npm install && grunt devel && DPNAME=-jm PORT=10002 pm2 startOrRestart ecosystem.config.js",
-              "post-deploy-local" : "lsof -t -i :8878 | xargs kill -9 ",
+              "post-deploy-local" : "lsof -t -i :8877 | xargs kill -9 ",
               "env" : {
                 "NODE_ENV": "staging",
               }
@@ -95,7 +95,7 @@ module.exports = {
               "path" : "/opt/kontext-ansa/deploy",
               "pre-deploy-local": "ssh -fN -L8877:kontext-dev:22 " + process.env.DPUSER + "@quest.ms.mff.cuni.cz",
               "post-deploy" : "npm install && grunt devel && DPNAME=-ansa PORT=10003 pm2 startOrRestart ecosystem.config.js",
-              "post-deploy-local" : "lsof -t -i :8878 | xargs kill -9 ",
+              "post-deploy-local" : "lsof -t -i :8877 | xargs kill -9 ",
               "env" : {
                 "NODE_ENV": "staging",
               }
