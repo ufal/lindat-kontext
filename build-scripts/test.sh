bb276769a2dff7090b42aae08f6de3120fa0fbb6
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2018-04-19 11:00:19 +0200

    build scripts

---
+++ build-scripts/test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e -o pipefail
+
+if [[ "x$INSTANCE_TEST" == "xtrue" ]]; then
+
+    URL=http://localhost:${PORT}
+
+    echo "============================================"
+    echo "Testing ${URL}/corpora/corplist"
+    curl -s ${URL}/corpora/corplist | grep -i ovm
+
+    declare -a urls=(
+    " "
+    "fullref?corpname=ovm_cs_w"
+    "get_cached_conc_sizes?corpname=ovm_cs_w"
+    "corpora/bibliography?corpname=ovm_cs_w"
+    )
+
+    # not working properly in 0.9 (not according to standard)
+    # curl -v http://localhost:5000/fcs/scan/?version=1.2&operation=explain
+
+    for q in "${urls[@]}"
+    do
+        echo "============================================"
+        echo "Testing ${URL}/${q}"
+        curl -vs ${URL}/${q}
+        echo
+    done
+
+    echo "============================================"
+    # simple blackbox fcs test
+    curl -vs "${URL}/fcs/v1?version=1.2&operation=searchRetrieve&query=klaus" | grep "fcs:DataView"
+    curl -vs "${URL}/fcs/v1?version=1.2&operation=scan&scanClause=fcs.resource=root" | grep "sru:displayTerm"
+    curl -vs "${URL}/fcs/v1?version=1.2&operation=explain" | grep "zr:title"
+
+
+    # =========================================================================
+    tail /opt/kontext/log/kontext.log || true
+
+fi
